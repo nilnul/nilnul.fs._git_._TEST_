4abb8b7154b813292299b7d0f2733a84586252b9
WANG,Youtian <wangyoutian@nilnul.com>
2023-06-08 15:33:31 +0800

    view repo such as creation time;etc;

---
+++ data/lastest/dnt/UnitTest1 - Copy.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections;
+using System.Linq;
+
+namespace nilnul.fs._git_._TEST_.data.latest.dnt
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+
+		public static _address._route.division_._div.DstI  GetDnt()
+		{
+			var appPath = nilnul.app_.dotnet_.dev._PathX.PrjBaseAsAddress_ofCalling();
+
+			var div = @"_data(!Git\230608\";
+
+			var shield = nilnul.fs.address_.shield_.baseDiv_.divInDivision_._BaseInAddressX.Create(
+				appPath, div
+			);
+
+			var link = nilnul.obj.str.to_.scalar_._MaxOrDefaultX.Max(
+				nilnul.fs.folder._Dsts1.Dsts(
+					nilnul.fs.folder._RegressionsX.ToFolder(
+					 nilnul.fs.Folder1.Of(
+						shield
+					)
+					 )
+				)
+				,
+				d=>	nilnul.fs.file.attr_.CreateTime.Get(
+					new nilnul.fs.address_.spear_.based_.Child(
+						shield
+						,
+						d.ToString()
+					).ToString()
+					
+				)
+						,
+				System.Collections.Generic.Comparer<DateTime>.Default
+			);
+
+			if (link is null)
+			{
+				throw new NullReferenceException();
+
+			}
+
+			return link;
+
+			
+
+		}
+	}
+}
---
+++ svr_/github/client/repo/vw/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace nilnul.fs._git_._TEST_.svr_.github.client.repo.vw
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			var acc = data.latest.dnt.UnitTest1.GetDnt().caption.ToString(); ///create the account where it's not pushed to remote for privacy reasons;
+
+			var svrRepoName = "nilnul.collection._axiomatic_._LIB_";
+
+			var client = new nilnul.fs.git.svr_.github.client_.Vaulted(
+				"git"
+				,
+				acc
+			); /// here you need to store the password in Windows Credential Manager first. Note the resource|url shall be composed accordingly to the code there; <see cref="nilnul.fs.git.svr_.github.client_.Vaulted.vaultKey"/>
+
+			var url = nilnul.fs.git.svr_.github.client.repo._VwX.Vw(
+				client.githubClient(), acc, svrRepoName
+			);
+
+
+		}
+	}
+}
